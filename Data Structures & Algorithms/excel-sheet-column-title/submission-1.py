8848100e386a0bec3f5716c0ab3822e9434e63ab
Sibi <85477603+t-sibiraj@users.noreply.github.com>
2026-09-10 16:43:24 +0800

    Add: excel-sheet-column-title - submission-1

---
+++ Data Structures & Algorithms/excel-sheet-column-title/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        coloumn_title = ""
+
+        while columnNumber > 0:
+            columnNumber -= 1
+            remainder, quotient  = columnNumber % 26, columnNumber // 26
+            coloumn_title += chr(ord('A') + remainder)
+            columnNumber = quotient
+
+
+        return coloumn_title[::-1]
+
+
+
+
+
+        
